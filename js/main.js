ea1683ee8896778a1a17ef9e8fe51d5b5573c2c8
Marcoantonio9 <marcojr918@gmail.com>
2020-04-09 18:08:37 -0300

    Netflix

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+    $("#botao1").click(function(){
+        $(".txtclique").toggle(1000);
+    })
+
+})
+
+$(function(){
+    $("#botao2").click(function(){
+        $(".txtclique2").toggle(1000);
+    })
+
+})
+
+$(function(){
+    $("#botao3").click(function(){
+        $(".txtclique3").toggle(1000);
+    })
+})
+
+$(function(){
+    $("#botao4").click(function(){
+        $(".txtclique4").toggle(1000);
+    })
+})
+
+$(function(){
+    $("#botao5").click(function(){
+        $(".txtclique5").toggle(1000);
+    })
+})
